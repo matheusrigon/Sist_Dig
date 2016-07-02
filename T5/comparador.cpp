b58ce8703044437c9a107d227e010ed34f04bd15
matheusrigon <matheusjacobirigon@gmail.com>
2016-07-02 14:16:38 -0300

    atualização

--- T5/comparador.cpp
+++ T5/comparador.cpp
@@ -1,14 +1,20 @@
 #include<stdio.h>
 
 int main(void) {
-  FILE *C = NULL, *VER = NULL, *OUTP = NULL;
+  FILE *C = NULL, *V = NULL, *saida = NULL;
   int out_ver, out_c, i;
-  VER = fopen("verilog.out", "r");
-  C = fopen("c.out", "r");
-  OUTP = fopen("cmp.out", "w");
-  for (i = 1; fscanf(VER, "%d", &out_ver) != EOF &&
-         fscanf(C, "%d", &out_c) != EOF; i++)
+  V = fopen("saidaV.txt", "r");
+  C = fopen("saidaC.txt", "r");
+  saida = fopen("saidaCompara.txt","w");
+
+
+  for (i = 1; fscanf(V, "%d", &out_ver) != EOF && fscanf(C, "%d", &out_c) != EOF; i++)
     if (out_ver != out_c)
-      fprintf(OUTP, "Valores diferentes para a saída na linha %d\n", i);
+      fprintf(saida, "Valores diferentes para a saida na linha %d\n", i);
+
+  fclose(V);
+  fclose(C);
+  fclose(saida);
+
   return 0;
 }
